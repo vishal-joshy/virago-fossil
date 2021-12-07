9edc843c0a9361db0d734c2e4071b05f5d201159
vishaljoshy <1812-vishal@protonmail.com>
2021-12-07 23:00:14 +0530

    Initial commit

---
+++ src/fIrebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsMQob1HPgBqiBfjgCNRdQdTXA41f7omc",
+  authDomain: "virago-4938d.firebaseapp.com",
+  projectId: "virago-4938d",
+  storageBucket: "virago-4938d.appspot.com",
+  messagingSenderId: "385652788383",
+  appId: "1:385652788383:web:044eb84b510ee06bd7e6aa"
+};
+
