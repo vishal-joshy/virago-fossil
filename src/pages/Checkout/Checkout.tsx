4046e48c841a8b162fdea95d0cbc4cb26368349d
vishaljoshy <1812-vishal@protonmail.com>
2021-12-10 19:56:12 +0530

    Add checkout page

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,27 @@
 import React, { ReactElement } from "react";
+import Layout from "../../components/Layout";
+import Subtotal from "../../components/Subtotal/Subtotal";
+import "./Checkout.css";
 
 interface Props {}
 
 function Checkout({}: Props): ReactElement {
-  return <div>Checkout</div>;
+  const shoppingBasket = "Your shopping basket";
+  return (
+    <Layout>
+      <div className="checkout">
+        <div className="checkout__left">
+          <img src="" alt="banner" className="checkout__banner" />
+          <div>
+            <h2 className="checkout__title">{shoppingBasket}</h2>
+          </div>
+        </div>
+        <div className="checkout__right">
+          <Subtotal />
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Checkout;
