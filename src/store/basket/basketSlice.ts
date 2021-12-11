97309dbd559ec797ee3860972460369978f655ab
vishaljoshy <1812-vishal@protonmail.com>
2021-12-11 14:52:12 +0530

    Modify removefrombasket to remove using index.

--- src/store/basket/basketSlice.ts
+++ src/store/basket/basketSlice.ts
@@ -1,28 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface BasketSlice {
-  items:any
+  items: any;
 }
 
-interface item{
-
-}
+interface item {}
 
 const initialState: BasketSlice = { items: [] };
 
 export const basketSlice = createSlice({
-    name:'basket',
-    initialState,
-    reducers:{
-        addToBasket:(state,action:PayloadAction<any>)=>{
-            state.items=[...state.items,action.payload];
-        },
-        removeFromBasket:(state,action:PayloadAction<any>)=>{
-            state.items=state.items.filter(action.payload)
-        }
-    }
-})
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action: PayloadAction<any>) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action: PayloadAction<any>) => {
+      const result = [...state.items];
+      result.splice(state.items.findIndex((item: any) => item.id === action.payload.id), 1);
+      state.items = [...result];
+    },
+  },
+});
 
-export const {addToBasket,removeFromBasket}= basketSlice.actions;
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 export default basketSlice.reducer;
