4bdea82d6e0dd7b1ace38f8ac0aea30bfdaefcff
vishaljoshy <1812-vishal@protonmail.com>
2021-12-11 14:52:32 +0530

    Add product to checkout.

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -2,11 +2,16 @@ import React, { ReactElement } from "react";
 import Layout from "../../components/Layout";
 import Subtotal from "../../components/Subtotal/Subtotal";
 import "./Checkout.css";
+import { useAppSelector, useAppDispatch } from "../../store/hooks";
+import Product from "../../components/Product/Product";
 
 interface Props {}
 
 function Checkout({}: Props): ReactElement {
+  const products = useAppSelector((state) => state.basket.items);
   const shoppingBasket = "Your shopping basket";
+  const findTotalAmount = (products: any[]) =>
+    products.reduce((acc, product) => acc + product.price, 0);
   return (
     <Layout>
       <div className="checkout">
@@ -14,10 +19,13 @@ function Checkout({}: Props): ReactElement {
           <img src="" alt="banner" className="checkout__banner" />
           <div>
             <h2 className="checkout__title">{shoppingBasket}</h2>
+            {products.map((product: any) => (
+              <Product data={product} type="checkout"></Product>
+            ))}
           </div>
         </div>
         <div className="checkout__right">
-          <Subtotal />
+          <Subtotal items={products.length} total={findTotalAmount(products)} />
         </div>
       </div>
     </Layout>
